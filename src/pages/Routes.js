423fe7d98bbc5e492f2cd392e8bc84b82e0dc269
Mevin G Monson <mevingmonson@gmail.com>
2022-11-15 12:03:10 +0530

    azure api test

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import Student from "./Student";
 import Dashboard from "./Dashboard";
+import AzureTest from "./Test/index";
 
 const RouteList = () => {
   return (
@@ -13,6 +14,7 @@ const RouteList = () => {
           {/* <Switch> */}
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Dashboard />} />
+          <Route path="test" element={<AzureTest />} />
           {/* </Switch> */}
         </Routes>
       </BrowserRouter>
